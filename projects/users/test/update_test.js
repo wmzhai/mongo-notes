0df9a23ec7b1d3ca5bb51f869d7688f01db47302
Weiming Zhai <wmzhai@gmail.com>
2017-03-15 07:16:49 +0800

    instance set&save and update

---
+++ projects/users/test/update_test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+  let joe;
+  beforeEach((done)=>{
+    joe = new User({name:'Joe'});
+    joe.save()
+      .then(()=>done());
+  });
+
+  function assertName(operation,done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
+  }
+
+  it('instance type using set&save', (done) => {
+    joe.set('name', 'Alex');
+    assertName(joe.save(),done);
+  });
+
+  it('A model instance can update', (done) => {
+    assertName(joe.update({'name': 'Alex'}),done);
+  });
+});
