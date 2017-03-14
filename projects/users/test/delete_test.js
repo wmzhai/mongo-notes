16671f4c5480235f969666e67c61cd3ed20df069
Weiming Zhai <wmzhai@gmail.com>
2017-03-15 00:18:47 +0800

    model instance remove

---
+++ projects/users/test/delete_test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+  let joe;
+
+  beforeEach((done)=>{
+    joe = new User({name: "Joe"});
+    joe.save()
+      .then(()=>done());
+  });
+
+  it('model instance remove',(done)=>{
+    joe.remove()
+      .then(() => User.findOne({name:'Joe'}))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
+
+  it('class method remove',()=>{
+
+  });
+
+  it('class method findAndRemove',()=>{
+
+  });
+
+  it('class method findByIdAndRemove',()=>{
+
+  });
+  
+});
