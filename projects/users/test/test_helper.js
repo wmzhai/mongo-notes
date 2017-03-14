d17bbdcfc816c07b80fd326657cc136225154853
Weiming Zhai <wmzhai@gmail.com>
2017-03-14 21:01:11 +0800

    setup test_helper

---
+++ projects/users/test/test_helper.js
@@ -0,0 +1,10 @@
+// 设置测试环境
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/users_test');
+mongoose.connection
+  .once('open', () => console.log('Good to go!'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
