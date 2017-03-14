c7cbf4d984fdf650c4fa934d2bb6e951ab082e7f
Weiming Zhai <wmzhai@gmail.com>
2017-03-15 06:54:13 +0800

    finish remove

--- projects/users/test/delete_test.js
+++ projects/users/test/delete_test.js
@@ -19,16 +19,34 @@ describe('Deleting a user', () => {
       });
   });
 
-  it('class method remove',()=>{
-
+  it('class method remove',(done) => {
+    //删除一些满足条件的数据
+    User.remove({name:'Joe'})
+      .then(() => User.findOne({name:'Joe'}))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
   });
 
-  it('class method findAndRemove',()=>{
-
+  it('class method findOneAndRemove',(done)=>{
+    // 删除一些满足条件的数据中的第一个
+    User.findOneAndRemove({name:'Joe'})
+      .then(() => User.findOne({name:'Joe'}))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
   });
 
-  it('class method findByIdAndRemove',()=>{
-
+  it('class method findByIdAndRemove',(done)=>{
+    //删除某个id的数据
+    User.findByIdAndRemove({_id: joe._id})
+      .then(() => User.findOne({name:'Joe'}))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
   });
   
 });
