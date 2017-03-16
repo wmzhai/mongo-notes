7dacf6a688d066b872876be635c4c7eb67e671c9
Weiming Zhai <wmzhai@gmail.com>
2017-03-16 11:24:20 +0800

    schema and model

--- projects/UpStarMusic/database/models/album.js
+++ projects/UpStarMusic/database/models/album.js
@@ -1,1 +1,15 @@
-// Todo: create Album Schema
+// Album只有Schema，没有Model，做为artist子数据存在
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+module.exports = AlbumSchema;
