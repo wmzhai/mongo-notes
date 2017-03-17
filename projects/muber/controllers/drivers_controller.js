82f765f9a62d804ce10884d1c308aea3735a318c
Weiming Zhai <wmzhai@gmail.com>
2017-03-17 18:00:01 +0800

    finish muber

--- projects/muber/controllers/drivers_controller.js
+++ projects/muber/controllers/drivers_controller.js
@@ -9,15 +9,15 @@ module.exports = {
     const driverProps = req.body;
     Driver.create(driverProps)
       .then(driver => res.send(driver))
-      .catch(next);
+      .catch(next)
   },
 
   edit(req, res, next) {
     const driverId = req.params.id;
     const driverProps = req.body;
 
-    Driver.findByIdAndUpdate({_id: driverId}, driverProps)
-      .then(() => Driver.findById({_id: driverId}))
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      .then(() => Driver.findById({ _id: id }))
       .then(driver => res.send(driver))
       .catch(next);
   },
@@ -27,6 +27,17 @@ module.exports = {
 
     Driver.findByIdAndRemove({ _id: driverId })
       .then(driver => res.status(204).send(driver))
+      .catch(next);
+  },
+
+  index(req, res, next) {
+    const { lng, lat } = req.query;
+
+    Driver.geoNear(
+      { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)] },
+      { spherical: true, maxDistance: 200000 }
+    )
+      .then(drivers => res.send(drivers))
       .catch(next);
   }
 }
