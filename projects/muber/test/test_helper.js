13ad3a8e447af8a2ca745a2082f304ed982f8c5b
Weiming Zhai <wmzhai@gmail.com>
2017-03-17 13:43:28 +0800

    NODE_ENV

---
+++ projects/muber/test/test_helper.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+before(done => {
+  mongoose.connect('mongodb://localhost/muber_test');
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', err => {
+      console.log('Warning', error);
+    });
+});
+
+beforeEach(done => {
+  const { drivers } = mongoose.connection.collections;
+  drivers.drop()
+    .then(() => done())
+    .catch(() => done());
+});
