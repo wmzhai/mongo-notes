ecf21064db60ece1c6bcf5b91c70a1589096e523
Weiming Zhai <wmzhai@gmail.com>
2017-03-17 15:44:46 +0800

    CRUD

--- projects/muber/app.js
+++ projects/muber/app.js
@@ -10,9 +10,11 @@ if(process.env.NODE_ENV !== 'test'){
   mongoose.connect('mongodb://localhost/muber');
 }
 
-
-
 app.use(bodyParser.json());
 routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({error: err.message});
+});
 
 module.exports = app;
--- projects/muber/test/controllers/drivers_controller_test.js
+++ projects/muber/test/controllers/drivers_controller_test.js
@@ -6,7 +6,7 @@ const app = require('../../app');
 const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
-  it('Post to /api/drivers creates a new driver', done => {
+  it('POST to /api/drivers creates a new driver', done => {
     Driver.count().then(count => {
       request(app)
         .post('/api/drivers')
@@ -16,6 +16,39 @@ describe('Drivers controller', () => {
             assert(count + 1 === newCount);
             done();
           })          
+        });
+    });
+  });
+
+  it('PUT to /api/drivers/id edits an existing driver', done => {
+    const driver = new Driver({email: 't@t.com', driving: false});
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({email: 't@t.com'})
+            .then(driver => {
+              assert(driver.driving === true);
+              done();
+            });
+        });
+    });
+  });
+
+  it('DELETE to /api/drivers/id can delete a driver', done => {
+    const driver = new Driver({email: 'test@test.com'});
+
+    driver.save().then(() => {
+      console.log(driver._id);
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+          Driver.findOne({email: 'test@test.com'})
+            .then( (driver) => {
+              assert( driver === null);
+              done();
+            })
         });
     });
   });
